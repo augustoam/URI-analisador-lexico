1220e2207778d2e2d8c5143908fdd440ef1f4fb6
augustoam <augusto.albertoni@gmail.com>
2017-11-15 17:26:40 -0200

    Novos ajustes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-//declaração das variáveis globais, assim como os tokens iniciais
 var colecaoPalavras = [];
 var Tabela = [];
 var palavraDigitada = $(".palavra-digitada");
@@ -6,76 +5,69 @@ var inputPalavra = $(".input-palavra");
 var campo = $("#campo-validacao");
 
 // Função para incluir as palavras na colecaoPalavras
-function adicionaColecaoPalavras () {
-  if ($(".input-palavra").val() > '') {
-    colecaoPalavras.push(inputPalavra.val());
-    $('#lista-palavras').append('<div class="chip">'+$(".input-palavra").val()+'<i class="close material-icons">close</i></div> ');
-    inserirPalavraNaTabela();
-    $(".input-palavra").val('');
-  } else {
-    Materialize.toast('Palavra não pode ser vazia!', 3000, 'rounded')
-  }
+function adicionaColecaoPalavras() {
+    if ($(".input-palavra").val() > '') {
+        colecaoPalavras.push(inputPalavra.val());
+        $('#lista-palavras').append('<div class="chip">' + $(".input-palavra").val() + '<i class="close material-icons">close</i></div> ');
+        inserirPalavraNaTabela();
+        $(".input-palavra").val('');
+    } else {
+        Materialize.toast('Palavra não pode ser vazia!', 3000, 'rounded')
+    }
 }
 
 $('#campo-validacao').keyup(function(e) {
-  inicializaMarcadores();
+    inicializaMarcadores();
 });
 
 function inserirPalavraNaTabela() {
-  insereTabela();
-  Tabela = linhas();
-	montaTabela(Tabela);
+    insereTabela();
+    Tabela = linhas();
+    montaTabela(Tabela);
 
-  $('#campo-validacao').keyup(function(event) {
-    if(Tabela.length > 0){
-      inicializaMarcadores(event);
-    }
-  });
+    $('#campo-validacao').keyup(function(event) {
+        if (Tabela.length > 0) {
+            inicializaMarcadores(event);
+        }
+    });
 }
 
 function inicializaMarcadores(event) {
-  var campoValidacao = $('#campo-validacao').val();
-  var a = 'a';
-  var z = 'z'
-  var estado = 0;
-  var errado = false;
+    var campoValidacao = $('#campo-validacao').val();
+    var a = 'a';
+    var z = 'z'
+    var linha = 0;
 
-  atualizaClassInputValidacao();
+    atualizaClassInputValidacao();
 
-  for (var i = 0; i < campoValidacao.length; i++) {
-		if(campoValidacao && errado == false){
-
-			marcaTabela(estado, campoValidacao[i]);
-      console.log(Tabela[estado][campoValidacao[i]]);
-			if(Tabela[estado][campoValidacao[i]] != '-') {
-				estado = Tabela[estado][campoValidacao[i]];
-			} else {
-				errado = true;
-			}
-		}
-	}
+    for (var i = 0; i < campoValidacao.length; i++) {
+        if (campoValidacao) {
+            marcaTabela(linha, campoValidacao[i]);
+            linha = Tabela[linha][campoValidacao[i]];
+        }
+    }
 }
 
-function atualizaClassInputValidacao(){
-  var campoValidacao = $('#campo-validacao').val();
+function atualizaClassInputValidacao() {
+    var campoValidacao = $('#campo-validacao').val();
 
-  if(campo.length == 1) {
-  	$('#tabela tr').removeClass('estado_selecionado');
-  	$('#tabela td').removeClass('letra_selecionada');
-  }
+    if (campo.length == 1) {
+        $('#tabela tr').removeClass('estado_selecionado');
+        $('#tabela td').removeClass('letra_selecionada');
+    }
 
-  if (jQuery.inArray(campoValidacao, colecaoPalavras) > -1) {
-    campo.addClass("certo");
-    campo.removeClass("errado");
-  } else {
-    campo.addClass("errado");
-    campo.removeClass("certo");
-  }
+    if (jQuery.inArray(campoValidacao, colecaoPalavras) > -1) {
+        campo.addClass("certo");
+        campo.removeClass("errado");
+    } else {
+        campo.addClass("errado");
+        campo.removeClass("certo");
+    }
 }
 
-function marcaTabela(linha, coluna){
-	$('#tabela tr').removeClass('estado_selecionado');
-	$('#tabela td').removeClass('letra_selecionada');
-	$('#tabela .estado_' + linha).addClass('estado_selecionado');
-	$('#tabela .caracter_' + coluna).addClass('letra_selecionada');
+function marcaTabela(linha, coluna) {
+    $('#tabela tr').removeClass('linha_selecao');
+    $('#tabela td').removeClass('palavra_selecao');
+    $('#tabela .estado_' + linha).addClass('linha_selecao');
+    $('#tabela .caracter_' + coluna).addClass('palavra_selecao');
 }
--- js/tabela.js
+++ js/tabela.js
@@ -1,94 +1,52 @@
-var estados = [[]];
-var geral = 0;
-
-function linhas(){
-  var estados_i = [];
-  var a = 'a';
-  var z = 'z';
-	for (var i = 0; i < estados.length; i++) {
-		var aux = [];
-		aux['estado'] = i;
-		for (var j = a.charCodeAt(0); j <= z.charCodeAt(0); j++) {
-			var caracter = String.fromCharCode(j);
-
-			if(typeof estados[i][caracter] === 'undefined'){
-
-        aux[caracter] = '-';
-			} else {
-				aux[caracter] = estados[i][caracter]
-			}
-		}
-		if(typeof estados[i]['estado_final'] !== 'undefined'){
-			aux['estado_final'] = true;
-		}
-		estados_i.push(aux);
-	}
-	return estados_i;
+function linhas() {
+    for (var a = [], i = 0; i < estados.length; i++) {
+        var e = [];
+        e.estado = i;
+        for (var codeAZ = "a".charCodeAt(0); codeAZ <= "z".charCodeAt(0); codeAZ++) {
+            var palavraAZ = String.fromCharCode(codeAZ);
+            if (typeof estados[i][palavraAZ] === 'undefined') {
+                e[palavraAZ] = "-";
+            } else {
+                e[palavraAZ] = estados[i][palavraAZ];
+            }
+        }
+        void 0 !== estados[i].estado_final && (e.estado_final = !0), a.push(e)
+    }
+    return a
 }
 
 function insereTabela() {
-  for (var i = 0; i < colecaoPalavras.length; i++) {
-
-		var estado_atual = 0;
-    var proximo_estado;
-		var palavra = colecaoPalavras[i];
-
-		for(var j = 0; j < palavra.length; j++){
-			if(typeof estados[estado_atual][palavra[j]] === 'undefined'){
-				proximo_estado = geral + 1;
-				estados[estado_atual][palavra[j]] = proximo_estado;
-				estados[proximo_estado] = [];
-				geral = estado_atual = proximo_estado;
-			} else {
-				estado_atual = estados[estado_atual][palavra[j]];
-			}
-
-      // Sinaliza o estado final para o estado atual
-			if(j == palavra.length - 1){
-				estados[estado_atual]['estado_final'] = true;
-			}
-		};
-	};
+    for (var a = 0; a < colecaoPalavras.length; a++)
+        for (var t, e = 0, o = colecaoPalavras[a], r = 0; r < o.length; r++) {
+            if (typeof estados[e][o[r]] === 'undefined') {
+                t = geral + 1;
+                estados[e][o[r]] = t;
+                estados[t] = [];
+                geral = e = t;
+            } else {
+                e = estados[e][o[r]];
+                r == o.length - 1 && (estados[e].estado_final = !0);
+            }
+        }
 }
 
-function montaTabela(estados_i){
-  tabela = $('#tabela');
-	tabela.html('');
-	var tr = $("<tr class=''>");
-	var th = $("<th>");
-	th.html('-');
-	tr.append(th);
-	var a = 'a';
-	var z = 'z';
-	for (var j = a.charCodeAt(0); j <= z.charCodeAt(0); j++) {
-		var th = $("<th>");
-		th.html(String.fromCharCode(j));
-		tr.append(th);
-	}
-	tabela.append(tr);
-
-	// Iteração
-	for(var i = 0; i < estados_i.length; i++){
-		var tr = $("<tr class='trestado'>");
-		var td = $("<td>");
-		if(estados_i[i]['final']){
-			td.html('q' + estados_i[i]['estado'] + '*');
-		} else {
-			td.html('q' + estados_i[i]['estado']);
-		}
-		tr.append(td);
-		tr.addClass('estado_'+estados_i[i]['estado']);
-		var a = 'a';
-		var z = 'z';
-		for (var j = a.charCodeAt(0); j <= z.charCodeAt(0); j++) {
-			var caracter = String.fromCharCode(j);
-			var td = $("<td>");
-			td.addClass('caracter_'+caracter);
-			if(estados_i[i][caracter] != '-'){
-				td.html('q' + estados_i[i][caracter]);
-			}
-			tr.append(td);
-		}
-		tabela.append(tr);
-	}
+function montaTabela(a) {
+    tabela = $("#tabela"), tabela.html("");
+    var t = $("<tr class=''>"),
+        header = $("<th>");
+    header.html("-"), t.append(header);
+    for (var o = "a", r = "z", d = o.charCodeAt(0); d <= r.charCodeAt(0); d++)(e = $("<th>")).html(String.fromCharCode(d)), t.append(e);
+    tabela.append(t);
+    for (var s = 0; s < a.length; s++) {
+        var t = $("<tr class='trestado'>"),
+            l = $("<td>");
+        a[s].final ? l.html("q" + a[s].estado + "*") : l.html("q" + a[s].estado), t.append(l), t.addClass("estado_" + a[s].estado);
+        for (var r = "z", d = (o = "a").charCodeAt(0); d <= r.charCodeAt(0); d++) {
+            var n = String.fromCharCode(d);
+            (l = $("<td>")).addClass("caracter_" + n), "-" != a[s][n] && l.html("q" + a[s][n]), t.append(l)
+        }
+        tabela.append(t)
+    }
 }
+
+var estados = [[]], geral = 0;
